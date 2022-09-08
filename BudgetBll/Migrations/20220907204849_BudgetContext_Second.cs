50dc5e2a1ce7e0072735055bb69d9fa78d5aba3d
Егор Павшук <egor.pavshuk@outlook.com>
2022-09-08 09:46:35 +0300

    Add new migration

---
+++ BudgetBll/Migrations/20220907204849_BudgetContext_Second.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetBll.Migrations
+{
+    public partial class BudgetContext_Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "LogsEntrie",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "LogsEntrie");
+        }
+    }
+}
